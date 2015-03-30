4d0691b82fcdf80c63c573698957ef90cf8630f3
XiyuChen <c272717012@gmail.com>
2015-03-30 22:22:19 +0800

    NetScan Finished

--- NetScan/NetScan.sh
+++ NetScan/NetScan.sh
@@ -4,20 +4,8 @@ function help
 {
 	exit 0;
 }
-#参数解析
-if [ $# -ne 1 ] ; then
-	echo "Bad parameter!";
-	help;
-fi
-IpAddr=($(echo $1 | sed 's/\// /;t;s/\\/ /'));
-Ip=${IpAddr[0]};
-NetMask=${IpAddr[1]};
-((NetField=$NetMask/8));
 
-Field1=$(echo $Ip | gawk 'BEGIN{FS="."}{print $1;}');
-Field2=$(echo $Ip | gawk 'BEGIN{FS="."}{print $2;}');
-Field3=$(echo $Ip | gawk 'BEGIN{FS="."}{print $3;}');
-Field4=$(echo $Ip | gawk 'BEGIN{FS="."}{print $4;}');
+IpList=$(./ParseIpAddr $1);
 
 trap exit 2 SIGINT;
 
@@ -40,16 +28,7 @@ PING()
 	
 }
 
-
-if [ $NetField -eq 3 ] ; then
-
-	#for(( LastNum=$Field4 ; LastNum<255 ; LastNum++ )){
-
-	# 	PING "$Field1.$Field2.$Field3.$LastNum"&
-
-	#}
-	for LastNum in `seq $Field4 254`;do
-	 	PING "$Field1.$Field2.$Field3.$LastNum"&
+	for Ip in $IpList ;do
+	 	PING "$Ip"&
 	done
-fi
 wait;
